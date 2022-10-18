33092e3a850f477ebb09bf972171098c29a417ad
Paul Jena <paul.jena@optimeas.de>
2022-10-18 14:32:08 +0200

    Refactor script to not use magic numbers

--- device-sim.py
+++ device-sim.py
@@ -3,20 +3,24 @@ import serial
 import time
 
 COM_PORT = '/dev/ttymxc2'
+SEND_AMOUNT = 5
+SEND_INTERVAL = 2
+COMMAND_STRING = b'h'
+BAUDRATE = 9600
 
 def getData():
     data = f'data:{time.time()}'
     return data.encode()
 
 def main():
-    com = serial.Serial(COM_PORT,baudrate=9600)
+    com = serial.Serial(COM_PORT,baudrate=BAUDRATE)
     
     while(True):
         character = com.read(1)
-        if character == b'h':
-            for i in range(5):
+        if character == COMMAND_STRING:
+            for i in range(SEND_AMOUNT):
                 com.write(getData())
-                time.sleep(2)
+                time.sleep(SEND_INTERVAL)
             com.flush()
             com.read_all()        
 
