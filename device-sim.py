8d981d4dc77b4159f2383b1a3dcea3b3cfd785b9
Paul Jena <paul.jena@optimeas.de>
2022-10-18 14:29:19 +0200

    Add first implemnation of write and read loop

--- device-sim.py
+++ device-sim.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 import serial
+import time
+
+COM_PORT = '/dev/ttymxc2'
+
+def getData():
+    data = f'data:{time.time()}'
+    return data.encode()
 
 def main():
-    print('This is the inital commit')
+    com = serial.Serial(COM_PORT,baudrate=9600)
+    
+    while(True):
+        character = com.read(1)
+        if character == b'h':
+            for i in range(5):
+                com.write(getData())
+                time.sleep(2)
+            com.flush()
+            com.read_all()        
 
 if __name__ == '__main__':
     main()
